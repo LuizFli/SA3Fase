e9053b72966450e91a3892184b1efe5e768d27f0
LuizFli <filipefln@gmail.com>
2025-05-26 20:40:12 -0300

    att

--- SA3Fase/src/components/BarraNavegacao.jsx
+++ SA3Fase/src/components/BarraNavegacao.jsx
@@ -1,87 +1,111 @@
 import React from 'react'
 import { Box, Stack, Typography } from '@mui/material'
 import { Link } from 'react-router-dom'
+import './BarraNavegacao.css'
 
 function BarraNavegacao() {
     return (
         <Box sx={{ width: '100%', height: '100%', backgroundColor: 'grey', display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
 
+            <Box sx={{display:'flex',flexDirection:'column' ,height:'20%', width:'100%', alignItems:'center'}}>
 
-            <Box>
-                <Stack className="icon-navbar">
-                    <img src="./public/imagens/icon2.png" alt="" />
-                </Stack>
-            </Box>
-
-            <Box>
-                <h2>Sale<span className="span-navbar">Sigth</span></h2>
-            </Box>
-
-
-            <Link to="/">
-                <Box sx={{ display: 'flex', flexDirection: 'row', border: '1px solid black', borderRadius: '5px' }} >
-
-                    <Stack className="iconesidebar">
-                        <img src="./imagens/Group 2.png" />
-                    </Stack>
-
-                    <Stack>
-                        <Typography className="textsidebar" sx={{ fontSize: '1.2rem', fontWeight: 'bold', color: '#000000' }}>Inicio</Typography>
-                    </Stack>
-
-                </Box>
-            </Link>
-
-            <Link to="/desempenho">
-                <Box sx={{ display: 'flex', flexDirection: 'row', border: '1px solid black', borderRadius: '5px' }}>
-                    <Stack className="iconesidebar">
-                        <img src="./imagens/TrendUp.png" />
-                    </Stack>
-                    <Stack>
-                        <Typography className="textsidebar" sx={{ fontSize: '1.2rem', fontWeight: 'bold', color: '#000000' }}>Funcionarios</Typography>
+                <Box>
+                    <Stack className="icon-navbar">
+                        <img src="./public/imagens/icon2.png" alt="" />
                     </Stack>
                 </Box>
-            </Link>
 
-            <Link to="/produtos">
-                <Box sx={{ display: 'flex', flexDirection: 'row', border: '1px solid black', borderRadius: '5px' }}>
-                    <Stack className="iconesidebar">
-                        <img src="./imagens/CirclesFour.png" alt="Produtos" />
-                    </Stack>
-                    <Stack>
-                        <Typography sx={{ fontSize: '1.2rem', fontWeight: 'bold', color: '#000000' }}>Produtos</Typography>
-                    </Stack>
+                <Box>
+                    <h2>Sale<span className="span-navbar">Sigth</span></h2>
                 </Box>
-            </Link>
-
-                <Link to="/maisVendidos">
-            <Box className="vendidos">
-                    <Box className="iconesidebar">
-                        <img src="./imagens/directions_car.png" alt="Mais Vendidos" />
-                    </Box>
-                    <Box className="textsidebar">Estoque</Box>
-            </Box>
-                </Link>
-
-            <Box className="config">
-                <Link to="/config">
-                    <Box className="iconesidebar">
-                        <img src="./imagens/Settings.png" alt="Cad Func" />
-                    </Box>
-                    <Box className="textsidebar">Configurações</Box>
-                </Link>
             </Box>
 
-            <Box className="logout">
-                <Link to="/login">
-                    <Box className="iconesidebar">
-                        <img src="./imagens/Log out.png" alt="Log Out" />
-                    </Box>
-                    <Box className="textsidebar">Log Out</Box>
-                </Link>
+            <Box sx={{display:'flex',flexDirection:'column' ,height:'80%', width:'100%', alignItems:'center', gap:'4%'}}>
+
+                <Box sx={{ width: '88%', border: '1px solid black', borderRadius: '5px', gap: '15%' }}>
+                    <Link to="/" className='link'>
+                        <Box sx={{ display: 'flex', flexDirection: 'row', gap: '4%', marginLeft: '2%' }} >
+
+                            <Stack className="iconesidebar">
+                                <img src="./imagens/Group 2.png" />
+                            </Stack>
+
+                            <Stack>
+                                <Typography className="textsidebar" sx={{ fontSize: '1.2rem', fontWeight: 'bold', color: '#000000' }}>Inicio</Typography>
+                            </Stack>
+
+                        </Box>
+                    </Link>
+                </Box>
+                <Box sx={{ width: '88%', border: '1px solid black', borderRadius: '5px', gap: '15%' }}>
+
+                    <Link to="/desempenho" className='link'>
+                        <Box sx={{ display: 'flex', flexDirection: 'row', gap: '4%', marginLeft: '2%' }}>
+                            <Stack className="iconesidebar">
+                                <img src="./imagens/TrendUp.png" />
+                            </Stack>
+                            <Stack>
+                                <Typography className="textsidebar" sx={{ fontSize: '1.2rem', fontWeight: 'bold', color: '#000000' }}>Funcionarios</Typography>
+                            </Stack>
+                        </Box>
+                    </Link>
+                </Box>
+
+                <Box sx={{ width: '88%', border: '1px solid black', borderRadius: '5px', gap: '15%' }}>
+                    <Link to="/produtos" className='link' >
+                        <Box sx={{ display: 'flex', flexDirection: 'row', gap: '4%', marginLeft: '2%' }}>
+                            <Stack className="iconesidebar">
+                                <img src="./imagens/CirclesFour.png" />
+                            </Stack>
+                            <Stack>
+                                <Typography sx={{ fontSize: '1.2rem', fontWeight: 'bold', color: '#000000', textDecoration: 'none' }}>Produtos</Typography>
+                            </Stack>
+                        </Box>
+                    </Link>
+                </Box>
+                <Box sx={{ width: '88%', border: '1px solid black', borderRadius: '5px', gap: '15%' }}>
+
+                    <Link to="/maisVendidos" className='link'>
+                        <Box sx={{ display: 'flex', flexDirection: 'row', gap: '4%', marginLeft: '2%' }}>
+                            <Stack className="iconesidebar">
+                                <img src="./imagens/directions_car.png" />
+                            </Stack>
+                            <Stack>
+                                <Typography sx={{ fontSize: '1.2rem', fontWeight: 'bold', color: '#000000' }}>Estoque</Typography>
+                            </Stack>
+                        </Box>
+                    </Link>
+                </Box>
+                <Box sx={{ width: '88%', border: '1px solid black', borderRadius: '5px', gap: '15%' }}>
+
+
+                    <Link to="/config" className='link'>
+                        <Box sx={{ display: 'flex', flexDirection: 'row', gap: '4%', marginLeft: '2%' }}>
+                            <Box className="iconesidebar">
+                                <img src="./imagens/Settings.png" />
+                            </Box>
+                            <Stack>
+                                <Typography sx={{ fontSize: '1.2rem', fontWeight: 'bold', color: '#000000' }}>Configurações</Typography>
+                            </Stack>
+                        </Box>
+                    </Link>
+                </Box>
+                <Box sx={{ width: '88%', border: '1px solid black', borderRadius: '5px', gap: '15%' }}>
+
+                    <Link to="/login" className='link'>
+                        <Box sx={{ display: 'flex', flexDirection: 'row', gap: '4%', marginLeft: '2%' }}>
+                            <Stack className="iconesidebar">
+                                <img src="./imagens/Log out.png" />
+                            </Stack>
+                            <Stack>
+                                <Typography sx={{ fontSize: '1.2rem', fontWeight: 'bold', color: '#000000' }}>Sair</Typography>
+                            </Stack>
+                        </Box>
+                    </Link>
+
+                </Box>
+
             </Box>
-
-
         </Box>
 
     )
