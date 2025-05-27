f8a57d6998f8db8718affdf05d8f4d9bcc3a2d84
ABILIO ALFREDO FRANCISCO <abilio_francisco@estudante.sesisenai.org.br>
2025-05-27 19:52:55 -0300

    alterações

--- SA3Fase/src/pages/CadastroFuncionario.jsx
+++ SA3Fase/src/pages/CadastroFuncionario.jsx
@@ -1,9 +1,18 @@
+import { Box } from '@mui/material'
 import React from 'react'
 
 function CadastroFuncionario() {
   return (
     <div>
-      <h1>Cadastro Funcionario</h1>
+
+      <Box sx={{p: 0, m: '0', width: '100vw', height: '100vh', backgroundColor: 'whitesmoke', display: 'flex', flexDirection: 'column'}}>
+
+        <Box sx={{p: '0', m: '0', width: '10vw', height: '100vh', backgroundColor: 'cyan', display: 'flex', flexDirection: 'column'}}>
+
+        </Box>
+
+      </Box>
+
     </div>
   )
 }
