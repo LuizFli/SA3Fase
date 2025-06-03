bfec11412082bff5273e32326590b05d5ab3d0df
LuizFli <filipefln@gmail.com>
2025-06-02 22:07:25 -0300

    att

--- Front_end/src/pages/Funcionarios.jsx
+++ Front_end/src/pages/Funcionarios.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
-import { Box, Container } from '@mui/material'
+import { Box, Button, Container } from '@mui/material'
 import PageContainer from '../components/PageContainer'
 import CadastroFuncionario from '../components/CadastroFuncionario'
+import { Navigate } from 'react-router'
 
 function Funcionarios() {
+
+  const handleCadastro = () => {
+    <Navigate to="/cadastroFuncionario" />
+  }
+  const handleLista = () => {
+    <Navigate to="/listaFuncionarios" />
+  }
+
   return (
     <PageContainer>
-          <CadastroFuncionario />       
+      <Button onClick={handleCadastro}>Cadastro Funcionario</Button>
+      <Button onClick={handleLista}>Lista Funcionarios</Button>
+      
+             
     </PageContainer>
   )
 }
