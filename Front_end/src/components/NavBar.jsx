28086d35d3fca8c6b493aaf3745af2498be8ee0f
JoabeSCosta <joabesantoscosta777@gmail.com>
2025-06-18 19:51:12 -0300

    add

--- Front_end/src/components/NavBar.jsx
+++ Front_end/src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Box, List, Divider, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography, styled, Card, CardMedia } from '@mui/material';
-import { Home, People, Apps, Settings, Logout, Edit } from '@mui/icons-material';
+import { Home, People, Apps, Settings, Logout, Edit, Work as CasesIcon } from '@mui/icons-material';
 import { Link, useLocation } from 'react-router-dom';
 
 const drawerWidth = 300;
@@ -56,9 +56,10 @@ export default function PermanentSidebar() {
   const menuItems = [
     { text: 'Inicio', icon: <Home />, path: '/dashboard' },
     { text: 'Funcionários', icon: <People />, path: '/funcionarios' },
+    { text: 'Gerenciar Acesso', icon: <CasesIcon />, path: '/gerenciaFun' },
     { text: 'Produtos', icon: <Apps />, path: '/produtos' },
     { text: 'Vendas', icon: <Edit />, path: '/vendas'},
-    { text: 'Configurações', icon: <Settings />, path: '/configuracoes' },
+    { text: 'Configurações', icon: <Settings />, path: '/gerenciaAcesso' },
     { text: 'Sair', icon: <Logout />, path: '/' },
   ];
 
