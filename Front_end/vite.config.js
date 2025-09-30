56dc092a1d63a1d1987df242bbf2e75b9c6cd009
JOABE DOS SANTOS COSTA <joabe_s_costa@estudante.sesisenai.org.br>
2025-09-29 21:51:59 -0300

    add

--- Front_end/vite.config.js
+++ Front_end/vite.config.js
@@ -6,6 +6,16 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: "0.0.0.0",
-    port:5173,
+    port: 5173,
   },
+  preview: {
+    host: "0.0.0.0",
+    port: 80,
+    proxy: {
+      '/api': {
+        target: 'http://backend:3000',
+        changeOrigin: true,
+      }
+    }
+  }
 });
